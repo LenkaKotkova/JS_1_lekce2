497dd4a7c251b365618b48348b81b17ec33f11cc
Lenka Kotková <lenka.nejedla@gmail.com>
2024-09-30 20:22:05 +0200

    hotovo

--- lekce_2_GITaGITHUB/script.js
+++ lekce_2_GITaGITHUB/script.js
@@ -4,3 +4,8 @@ const pocetPredstaveni = 15
 const prijem = cenaListku * pocetNavstevniku * pocetPredstaveni
 document.body.innerHTML += "<p> <h2>" + "Měsíční příjem divadla: " + prijem + " Eur" + "</h2></p>"
 console.log("jmenuji se Lenka")
+const studentskeVstupne = cenaListku * 0.65
+const pocetStudentu = pocetNavstevniku * 0.4
+const vyslednyPrijem = studentskeVstupne * 15 * pocetStudentu
+console.log(vyslednyPrijem)
+document.body.innerHTML += "<p> <h2>" + "Měsíční příjem divadla se studentskou slevou: " + vyslednyPrijem + " Eur" + "</h2></p>"
