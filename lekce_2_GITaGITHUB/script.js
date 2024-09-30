d1c8cbac645ade58061b5d232c2a69eefcf18207
Lenka Kotková <lenka.nejedla@gmail.com>
2024-09-30 20:08:47 +0200

    divadlo poprve

---
+++ lekce_2_GITaGITHUB/script.js
@@ -0,0 +1,6 @@
+const cenaListku = 12
+const pocetNavstevniku = 174
+const pocetPredstaveni = 15
+const prijem = cenaListku * pocetNavstevniku * pocetPredstaveni
+document.body.innerHTML += "<p> <h2>" + "Měsíční příjem divadla: " + prijem + " Eur" + "</h2></p>"
+console.log("jmenuji se Lenka")
